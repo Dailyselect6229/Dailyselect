e3212060365bcd9d88bac6590ed7e05722ff326f
Noushad Shaikh <13shaikhsalman@gmail.com>
2025-08-10 22:42:38 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1); // Stop app if DB not connected
+  }
+};
+
+module.exports = connectDb;
